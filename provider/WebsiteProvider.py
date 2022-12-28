371aa72948d843a0dadf3f07ffa1db65ccafd379
Bob74 <olivierfouchecourt@gmail.com>
2022-12-28 01:18:23 +0100

    Lecture du fichier html avec l'encodage UTF-8

--- provider/WebsiteProvider.py
+++ provider/WebsiteProvider.py
@@ -13,6 +13,6 @@ class WebsiteProvider:
         @self.__app.get("/")
         async def get():
             self.__app.mount("/static", StaticFiles(directory="www/static"), name="static")
-            with open("www/index.html") as file:
+            with open("www/index.html", encoding="UTF-8") as file:
                 html_read = file.read()
             return HTMLResponse(html_read)
