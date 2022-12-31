08f4f07c347fc8000779290f3cd799e3462cd8da
Bob74 <olivierfouchecourt@gmail.com>
2022-12-31 19:12:48 +0100

    Ajout de la grille + Block d'information en haut à droite

--- www/static/js/backgroundMap.js
+++ www/static/js/backgroundMap.js
@@ -7,8 +7,51 @@ export default class BackgroundMap {
         this.backgroundHeight = 0;
         this.backgroundWidth = 0;
         this.backgroundContainer = document.querySelector(`#${backgroundContainerId}`);
-        this.backgroundImage = this.backgroundContainer.querySelector("img");
-        this.backgroundImage.onmousemove = this.onMouseMove.bind(this);
+        this.backgroundGrid = this.backgroundContainer.querySelector("#background-grid");
+        this.backgroundImage = this.backgroundContainer.querySelector("#background-map");
+
+        this.createGrid();
+    }
+
+    /*
+    * Créé la grille de fond qui contient les coordonnées des cellules.
+    */
+    createGrid() {
+        this.backgroundMinX = -91;
+        this.backgroundMaxX = 43;
+        this.backgroundMinY = -120;
+        this.backgroundMaxY = 48;
+
+        this.backgroundGridBody = document.createElement("tbody");
+
+        for (let y = this.backgroundMinY; y <= this.backgroundMaxY; y++) {
+            let column = this.backgroundGridBody.appendChild(document.createElement("tr"));
+            for (let x = this.backgroundMinX; x <= this.backgroundMaxX; x++) {
+                let cell = document.createElement("td");
+                cell.title = `${x}:${y}`;
+                cell.setAttribute("cell-x", x);
+                cell.setAttribute("cell-y", y);
+                column.appendChild(cell);
+            }
+        }
+
+        this.backgroundGrid.appendChild(this.backgroundGridBody);
+        this.backgroundContainer.appendChild(this.backgroundGrid);
+    }
+
+    /*
+    * Créé l'image de fond.
+    */
+    setBackground(bgPath, height, width) {
+        this.backgroundFilePath = bgPath;
+        this.backgroundHeight = height;
+        this.backgroundWidth = width;
+        this.backgroundImage.style.setProperty("background", `url('${this.backgroundFilePath}') no-repeat`);
+        this.backgroundImage.style.setProperty("height", `${this.backgroundHeight}px`);
+        this.backgroundImage.style.setProperty("width", `${this.backgroundWidth}px`);
+
+        this.backgroundGrid.style.setProperty("height", `${this.backgroundHeight}px`);
+        this.backgroundGrid.style.setProperty("width", `${this.backgroundWidth}px`);
     }
 
     getBackgroundHeight() {
@@ -19,16 +62,4 @@ export default class BackgroundMap {
         return this.backgroundWidth;
     }
 
-    setBackground(bgPath, height, width) {
-        this.backgroundFilePath = bgPath;
-        this.backgroundHeight = height;
-        this.backgroundWidth = width;
-        this.backgroundImage.style.setProperty("background", `url('${this.backgroundFilePath}') no-repeat`);
-        this.backgroundImage.style.setProperty("height", `${this.backgroundHeight}px`);
-        this.backgroundImage.style.setProperty("width", `${this.backgroundWidth}px`);
-    }
-
-    onMouseMove(event) {
-        this.game.setInfoCoordinates(event.offsetX, event.offsetY);
-    }
 }
